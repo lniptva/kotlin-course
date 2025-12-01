099d5ce5123966d69089499b20cd84721720fe7a
lniptva <pl.iptva@gmail.com>
2025-12-01 19:44:51 +0100

    new file for homework has been added

---
+++ src/main/kotlin/kotlin_course/lessons/lesson23/homework/Task 3.kt
@@ -0,0 +1,27 @@
+package ru.stimmax.kotlin_course.lessons.lesson23.homework
+
+// Task 3
+
+fun List<Int>.duplicates(): Set<Int> {
+    return toSet().associate { element ->
+        element to this.count { it == element }
+    }.filter { it.value > 1 }
+        .keys
+}
+
+fun main() {
+    val d1 = fun List<Int>.(): Set<Int> {
+        return toSet().associate { element ->
+            element to this.count { it == element }
+        }.filter { it.value > 1 }
+            .keys
+    }
+    val d11: List<Int>.() -> Set<Int> = {
+        toSet().associate { element ->
+            element to this.count { it == element }
+        }.filter { it.value > 1 }
+            .keys
+    }
+    val l = listOf(0, 1, 2, 4, 2, 5, 6, 5, 7)
+    println(l.d11())
+}
