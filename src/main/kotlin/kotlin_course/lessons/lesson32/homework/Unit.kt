bb68bd5f450b039d82257a583e00e5f1d006d874
lniptva <pl.iptva@gmail.com>
2025-12-28 14:04:18 +0100

    new file for homework has been added

---
+++ src/main/kotlin/kotlin_course/lessons/lesson32/homework/Unit.kt
@@ -0,0 +1,35 @@
+package ru.stimmax.kotlin_course.lessons.lesson32.homework
+
+class Unitclass PhoneNumberFormatterTest {
+
+    @ParameterizedTest
+    @MethodSource("validPhones")
+    fun formatValidPhonesTest(source: String, expected: String) {
+        val formatter = PhoneNumberFormatter()
+        val actual = formatter.formatPhoneNumber(source)
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["12345", "+1 (922) 941-11-11"])
+    fun invalidPhoneTest(source: String) {
+        val formatter = PhoneNumberFormatter()
+        assertThrows(IllegalArgumentException::class.java) {
+            formatter.formatPhoneNumber(source)
+        }
+    }
+
+
+    companion object {
+
+        @JvmStatic
+        fun validPhones() = listOf<Any>(
+            arrayOf("8 (922) 941-11-11", "+7 (922) 941-11-11"),
+            arrayOf("79229411111", "+7 (922) 941-11-11"),
+            arrayOf("+7 922 941 11 11", "+7 (922) 941-11-11"),
+            arrayOf("9229411111", "+7 (922) 941-11-11"),
+            arrayOf("abc +7 922 941 11 11", "+7 (922) 941-11-11"),
+        )
+    }
+} {
+}
